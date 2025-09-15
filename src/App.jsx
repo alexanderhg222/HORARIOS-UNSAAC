b3e98a5afb7579fed1a469fec2173c5b79ed20b9
alexanderhg222 <alexanderhuamanguevara@gmail.com>
2025-09-15 15:08:13 -0500

    last

--- src/App.jsx
+++ src/App.jsx
@@ -250,9 +250,8 @@ function App() {
         
         <div className="flex flex-col gap-4">
           <label className="font-semibold">Seleccione su carrera:</label>
-          {loading ? (
-            <SmallLoader message="Cargando carreras..." />
-          ) : (
+          {loading && <FullScreenLoader message="Cargando carreras..." />}
+          {!loading && (
             <select
               className="border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
               value={carrera}
